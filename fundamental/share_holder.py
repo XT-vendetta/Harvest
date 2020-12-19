ec5602ecd36780a5a24a6e319b285b54f71dcd58
XIE TIAN <xt_vendetta@outlook.com>
2020-12-19 21:46:17 +0800

    web crawler for share holder information #1
    support non circulate share holder data fetching

--- fundamental/share_holder.py
+++ fundamental/share_holder.py
@@ -1,6 +1,6 @@
 import requests
 from datetime import date, datetime
-from typing import Union, List
+from typing import List
 from pandas import DataFrame
 
 from core.equity import Equity
@@ -19,10 +19,21 @@ class ShareHolderItem(dict):
     @classmethod
     def from_tr_node(cls, tr_node):
         l: list = tr_node.find_all("td")
+
+        if l[2].text[-1] in ['↑', '↓']:
+            parsed_l2 = l[2].text[:-1]
+        else:
+            parsed_l2 = l[2].text
+
+        if l[3].text[-1] in ['↑', '↓']:
+            parsed_l3 = l[3].text[:-1]
+        else:
+            parsed_l3 = l[3].text
+
         return cls(
             l[1].text,
-            int(l[2].text),
-            float(l[3].text),
+            int(parsed_l2),
+            float(parsed_l3),
             l[4].text,
         )
 
@@ -32,53 +43,83 @@ class ShareHolder:
                  equity: Equity = None,
                  due_date: date = None,
                  report_date: date = None,
+                 share_holder_count: int = None,
                  share_holder_df: DataFrame = None):
         self.equity = equity
         self.due_date = due_date
         self.report_date = report_date
+        self.share_holder_count = share_holder_count
         self.share_holder_df = share_holder_df
 
-    @classmethod
-    def get_response_from_website(cls, equity: Equity, detailed: bool = False):
-        if detailed:
-            url = f'https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CirculateStockHolder/stockid/{equity.id}.phtml'
-        else:
-            url = 'https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CirculateStockHolder/stockid/' \
-                  + f'{equity.id}/displaytype/30.phtml'
+    @staticmethod
+    def get_request_url(equity: Equity, is_circulate: bool = True, detailed: bool = False):
+        circulate_str = 'vCI_CirculateStockHolder' if is_circulate else 'vCI_StockHolder'
+        detailed_str = '' if detailed else '/displaytype/30'
+        return f'https://vip.stock.finance.sina.com.cn/corp/go.php/{circulate_str}/stockid/{equity.id}{detailed_str}.phtml'
+
+    @staticmethod
+    def get_response_from_website(equity: Equity, is_circulate: bool = True, detailed: bool = False):
+        url = ShareHolder.get_request_url(equity, is_circulate, detailed)
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
-        return soup.find("table", {"id": "CirculateShareholderTable"})
+        if is_circulate:
+            return soup.find("table", {"id": "CirculateShareholderTable"})
+        else:
+            return soup.find("table", {"id": "Table1"})
 
     @classmethod
-    def from_website(cls, equity, due_date: date = None, detailed = False) -> Union["ShareHolder", List["ShareHolder"]]:
-        share_holder_table = ShareHolder.get_response_from_website(equity, detailed)
+    def from_website(
+            cls,
+            equity: Equity,
+            due_date: date = None,
+            is_circulate: bool = True,
+            detailed: bool = False) -> List["ShareHolder"]:
+        share_holder_table = ShareHolder.get_response_from_website(equity, is_circulate, detailed)
         all_tr = share_holder_table.find_all("tr")
         return_value = []
         for i in range(len(all_tr)):
-            tr = all_tr[i]
-            first_td = tr.find("td")
-            if first_td is None:
+            due_date_tr = all_tr[i]
+            due_date_td = due_date_tr.find("td")
+            if due_date_td is None:
                 continue
-            strong_node = first_td.find("strong")
+            strong_node = due_date_td.find("strong")
             if strong_node is not None and len(strong_node) > 0:
-                if strong_node.text == "截止日期":
-                    due_date_test = datetime.strptime(
-                        tr.find("td", {"class": "tdr", "colspan": "5"}).text,
-                        "%Y-%m-%d").date()
-                    if due_date == due_date_test or due_date is None:
+                if (is_circulate and strong_node.text == "截止日期") or (not is_circulate and strong_node.text == "截至日期"):
+                    actual_due_date = datetime.strptime(due_date_td.find_next("td").text, "%Y-%m-%d").date()
+                    if due_date == actual_due_date or due_date is None:
+                        report_date_tr = all_tr[i + 1]
+                        report_date_td = report_date_tr.find("td")
+                        assert report_date_td.text == "公告日期", "Current TR is not report date TR."
                         try:
-                            report_date = datetime.strptime(
-                                all_tr[i+1].find("td", {"class": "tdr", "colspan": "5"}).text,
-                                "%Y-%m-%d").date()
+                            report_date = datetime.strptime(report_date_td.find_next("td").text, "%Y-%m-%d").date()
                         except ValueError:
                             # old data does not have report date
                             report_date = None
 
-                        share_holder_records = []
-                        for j in range(10):
-                            share_holder_records.append(ShareHolderItem.from_tr_node(all_tr[i+3+j]))
+                        if not is_circulate:
+                            share_holder_count_tr = all_tr[i + 3]
+                            share_holder_count_td = share_holder_count_tr.find("td")
+                            assert share_holder_count_td.text == "股东总数", "Current TR is not share holder count TR."
+                            share_holder_count_text = share_holder_count_td.find_next("td").text
+                            try:
+                                share_holder_count = int(share_holder_count_text)
+                            except ValueError:
+                                assert share_holder_count_text[-6:] == "查看变化趋势"
+                                share_holder_count = int(share_holder_count_text[:-6])
+                        else:
+                            share_holder_count = None
+
+                        if is_circulate:
+                            share_holder_records = [ShareHolderItem.from_tr_node(all_tr[i + 3 + j]) for j in range(10)]
+                        else:
+                            share_holder_records = [ShareHolderItem.from_tr_node(all_tr[i + 6 + j]) for j in range(10)]
+
                         return_value.append(
-                            cls(equity, due_date_test, report_date, DataFrame.from_records(share_holder_records)))
+                            cls(equity,
+                                actual_due_date,
+                                report_date,
+                                share_holder_count,
+                                DataFrame.from_records(share_holder_records)))
                         if due_date is not None:
                             break
         return return_value
--- unit_test/share_holder.py
+++ unit_test/share_holder.py
@@ -45,10 +45,41 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(target_share_holders, share_holder.share_holder_df.to_dict('records'))
 
     def test_from_website_3(self):
+        equity = Equity(603290, "斯达半导")
+        share_holder = ShareHolder.from_website(equity, date(2020, 9, 30), False)[0]
+        target_share_holders = [
+            {'name': '香港斯达控股有限公司', 'quantity': 71266800, 'pct': 44.54, 'share_holder_type': '限售流通股'},
+            {'name': '浙江兴得利纺织有限公司', 'quantity': 29294388, 'pct': 18.31, 'share_holder_type': '限售流通股'},
+            {'name': '嘉兴富瑞德投资合伙企业(有限合伙)', 'quantity': 8684964, 'pct': 5.43, 'share_holder_type': '限售流通股'},
+            {'name': '嘉兴兴泽投资合伙企业(有限合伙)', 'quantity': 5899296, 'pct': 3.69, 'share_holder_type': '限售流通股'},
+            {'name': '中国工商银行股份有限公司－融通中国风1号灵活配置混合型证券投资基金', 'quantity': 1146295, 'pct': 0.72, 'share_holder_type': '流通A股'},
+            {'name': '宁波展兴投资有限公司', 'quantity': 1099644, 'pct': 0.69, 'share_holder_type': '限售流通股'},
+            {'name': '招商银行股份有限公司－银河创新成长混合型证券投资基金', 'quantity': 900891, 'pct': 0.56, 'share_holder_type': '流通A股'},
+            {'name': '深圳市鑫亮五金制品有限公司', 'quantity': 887856, 'pct': 0.55, 'share_holder_type': '限售流通股'},
+            {'name': 'MORGAN STANLEY&CO.INTERNATIONAL PLC.', 'quantity': 884864, 'pct': 0.55, 'share_holder_type': '流通A股'},
+            {'name': '王君', 'quantity': 830335, 'pct': 0.52, 'share_holder_type': '流通A股'}
+        ]
+
+        self.assertEqual(date(2020, 9, 30), share_holder.due_date)
+        self.assertEqual(date(2020, 10, 30), share_holder.report_date)
+        self.assertEqual(target_share_holders, share_holder.share_holder_df.to_dict('records'))
+
+    def test_from_website_4(self):
         equity = Equity(600028, "中国石化")
-        share_holder_1 = ShareHolder.from_website(equity, None, True)
-        share_holder_2 = ShareHolder.from_website(equity, None, False)
+        share_holder_1 = ShareHolder.from_website(equity, None, True, True)
+        share_holder_2 = ShareHolder.from_website(equity, None, True, False)
         self.assertNotEqual(len(share_holder_1), len(share_holder_2))
+
+    def test_get_request_url(self):
+        equity = Equity(600028, "中国石化")
+        url1 = ShareHolder.get_request_url(equity, True, True)
+        url2 = ShareHolder.get_request_url(equity, False, True)
+        url3 = ShareHolder.get_request_url(equity, True, False)
+        url4 = ShareHolder.get_request_url(equity, False, False)
+        self.assertEqual(url1, 'https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CirculateStockHolder/stockid/600028.phtml')
+        self.assertEqual(url2, 'https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_StockHolder/stockid/600028.phtml')
+        self.assertEqual(url3, 'https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CirculateStockHolder/stockid/600028/displaytype/30.phtml')
+        self.assertEqual(url4, 'https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_StockHolder/stockid/600028/displaytype/30.phtml')
 
 
 if __name__ == '__main__':
